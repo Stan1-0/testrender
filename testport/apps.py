48b4dea00d06f154c7c13b2f852f6f4f2a767d0d
Stan1-0 <stankofb@gmail.com>
2025-03-13 17:20:36 +0000

    created an app

---
+++ testport/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TestportConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'testport'
---
+++ testport/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    Company = models.CharField(max_length=40)
+    Project_name = models.CharField(max_length=50)
+    year = models.CharField(max_length=4)  # Assuming year is a 4-digit number
+    Project_photo = models.ImageField(upload_to='images')
+    live_site_url = models.URLField()
+    
+    def __str__(self):
+        return self.Project_name
+    
+class Description(models.Model):
+    project = models.ForeignKey(Project, related_name='results', on_delete=models.CASCADE)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.description
