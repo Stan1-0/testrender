fe8edec0ebbbb402e42df0beb6189d88fc1dfdc8
Stan1-0 <stankofb@gmail.com>
2025-03-13 17:56:35 +0000

    "Added DescriptionInline and ProjectAdmin classes to testport/admin.py"

--- testport/admin.py
+++ testport/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class DescriptionInline(admin.TabularInline):
+    model = Description
+    extra = 1  # Number of extra forms to display
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [DescriptionInline]
+    list_display = ('Project_name', 'Company', 'year')
+    search_fields = ('Project_name', 'Company')
+    
+admin.site.register(Project, ProjectAdmin)
